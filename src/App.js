d889125ff461942263e98cbadf78cac5d5ea24e7
Sebastian Crossa <crossasebastian@gmail.com>
2019-02-24 18:25:48 -0700

    Started with the base of the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Works</h1>
+        <Header />
+        <Contact />
       </div>
     );
   }
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Sebastian Crossa</h4>
+                <ul>
+                    <li>Email: sebastian@crossa.com</li>
+                    <li>Phone: +52 (667) 755 9877</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Contact;
