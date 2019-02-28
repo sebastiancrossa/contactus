c6f0877d84a7cd2991fdc6f8ab090f7343d83389
Sebastian Crossa <crossasebastian@gmail.com>
2019-02-27 22:12:48 -0600

    Added bootstrap and changed up the UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +10,11 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Sebastian's Contact Manager" />
-        <Contact name="Sebastian Crossa" email="crossasebastian@gmail.com" phoneNumber="+52 (667) 755 9878" />
-        <Contact name="Joe Bargain" email="jbargain@gmail.com" phoneNumber="+52 (622) 571 3042" />
 
-        <Contact />
+        <div className="container">
+          <Contact name="Sebastian Crossa" email="crossasebastian@gmail.com" phoneNumber="+52 (667) 755 9878" />
+          <Contact name="Joe Bargain" email="jbargain@gmail.com" phoneNumber="+52 (622) 571 3042" />
+        </div>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,18 @@ const Header = (props) => {
     const { branding } = props;
 
     return (
-        <div>
-            <h1 style={headingStyle}>{branding}</h1>
-        </div>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+            <div className="container">
+                <a href="/" className="navbar-brand">{branding}</a>
+                <div>
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-ie">
+                            <a href="/" className="nav-link">Home</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
 
