7ec12bb54e21c7e5e6d303ae0efbae0f5870b004
Sebastian Crossa <crossasebastian@gmail.com>
2019-02-27 21:53:20 -0600

    Added defaultProps and PropTypes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header branding="Sebastian's Contact Manager" />
+        <Contact name="Sebastian Crossa" email="crossasebastian@gmail.com" phoneNumber="+52 (667) 755 9878" />
+        <Contact name="Joe Bargain" email="jbargain@gmail.com" phoneNumber="+52 (622) 571 3042" />
+
         <Contact />
       </div>
     );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Contact extends Component {
     render() {
+        const { name, email, phoneNumber } = this.props;
+
         return (
             <div>
-                <h4>Sebastian Crossa</h4>
+                <h4>{name}</h4>
                 <ul>
-                    <li>Email: sebastian@crossa.com</li>
-                    <li>Phone: +52 (667) 755 9877</li>
+                    <li>{email}</li>
+                    <li>{phoneNumber}</li>
                 </ul>
             </div>
         )
     }
 }
+
+Contact.defaultProps = {
+    name: 'Name here',
+    email: 'Email here',
+    phoneNumber: 'Phone Number here'
+};
+
+Contact.propType = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phoneNumber: PropTypes.string.isRequired
+};
 
 export default Contact;
