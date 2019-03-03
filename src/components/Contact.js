7b860c34c6f59350f930b42b61426ca54c1b08ad
Sebastian Crossa <crossasebastian@gmail.com>
2019-03-03 15:30:13 -0600

    Contacts are now removable

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,13 +6,25 @@ class Contact extends Component {
         showContactInfo: false
     };
 
+    onDeleteClick = () => {
+        this.props.deleteClickHandler();
+    }
+
     render() {
         const { contact } = this.props;
         const { showContactInfo } = this.state;
 
         return (
             <div className="card card-body mb-3 ml-3 mr-3">
-                <h4>{contact.name} <i onClick={() => this.setState({ showContactInfo: !showContactInfo })} className="fas fa-sort-down" /></h4>
+                <h4>{contact.name}
+                    <i
+                        onClick={() => this.setState({ showContactInfo: !showContactInfo })} className="fas fa-sort-down" style={{ cursor: 'pointer' }} />
+
+                    <i
+                        className="fas fa-times" style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+                        onClick={this.onDeleteClick}
+                    />
+                </h4>
 
                 {showContactInfo ? (<ul className="list-group">
                     <li className="list-group-item">{contact.email}</li>
@@ -31,7 +43,8 @@ Contact.defaultProps = {
 };
 
 Contact.propType = {
-    contact: PropTypes.object.isRequired
+    contact: PropTypes.object.isRequired,
+    deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
